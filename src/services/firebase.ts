bf93a26b7849037ee8cf6bba9196bbba09cf43b1
Wener Rodrigo <wener19.wr@gmail.com>
2026-02-10 10:01:36 -0300

    feat: conect with firebase

---
+++ src/services/firebase.ts
@@ -0,0 +1,34 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics, isSupported } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC4rw_vj3vXheaNUG1RYhaMcT2W3TtM6Wk",
+  authDomain: "ribeiraocambio.firebaseapp.com",
+  projectId: "ribeiraocambio",
+  storageBucket: "ribeiraocambio.firebasestorage.app",
+  messagingSenderId: "968101998412",
+  appId: "1:968101998412:web:e67ba15ed3c86a3af2e9e9",
+  measurementId: "G-PJWTJXBR9L",
+};
+
+// Inicializa o app
+const app = initializeApp(firebaseConfig);
+
+// Serviços Firebase
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
+
+// Analytics (só funciona no browser)
+export let analytics: any = null;
+
+isSupported().then((supported) => {
+  if (supported) {
+    analytics = getAnalytics(app);
+  }
+});
+
+export default app;
